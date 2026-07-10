6125b2f58db3f025ba49bce6e126bc7ec7f2a36d
SuwillWD <136084114+SuwillWD@users.noreply.github.com>
2026-07-10 18:25:22 +0530

    Add buildTree and prettyPrint method
    buildTree generate a bst from an array. prettyPrint visualizes the tree on the console.

--- src/bst.js
+++ src/bst.js
@@ -6,6 +6,38 @@ function Node() {
   return { left, data, right };
 }
 
-function Tree() {}
+function Tree(array) {
+  // Sort array in ascending order and remove duplicates
+  let refinedArray = [...new Set(array.sort((a, b) => a - b))];
+  let root = buildTree(refinedArray, 0, refinedArray.length - 1);
+
+  function buildTree(arr, start, end) {
+    if (start > end) {
+      return;
+    }
+    let mid = Math.floor((start + end) / 2);
+    let rootNode = Node();
+    rootNode.data = arr[mid];
+    rootNode.left = buildTree(arr, start, mid - 1);
+    rootNode.right = buildTree(arr, mid + 1, end);
+
+    return rootNode;
+  }
+
+  // Prints tree into the console
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  };
+
+  const print = () => prettyPrint(root);
+
+  return { print };
+}
 
 export default Tree;
